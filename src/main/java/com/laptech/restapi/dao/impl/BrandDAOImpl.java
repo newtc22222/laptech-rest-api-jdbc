d6296a0f0f34b3455d6a2415095d296f75edad74
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-02-08 23:05:54 +0700

    [DAO - Main] Update PropertySource for BrandDAOImpl

--- src/main/java/com/laptech/restapi/dao/impl/BrandDAOImpl.java
+++ src/main/java/com/laptech/restapi/dao/impl/BrandDAOImpl.java
@@ -29,7 +29,7 @@ import java.util.Objects;
 @Transactional
 @Log4j2
 @Component
-@PropertySource("query.properties")
+@PropertySource("classpath:query.properties")
 public class BrandDAOImpl implements BrandDAO {
     @Autowired
     private JdbcTemplate jdbcTemplate;
