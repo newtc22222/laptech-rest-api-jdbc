b3152f20c158982f90d1aed9d369430bf4107dfa
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-02-08 22:50:33 +0700

    [DTO - Response] Create class for get new access token

---
+++ src/main/java/com/laptech/restapi/jwt/dto/TokenRefreshResponse.java
@@ -0,0 +1,19 @@
+package com.laptech.restapi.jwt.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @since 2023-02-08
+ */
+@Getter @Setter
+public class TokenRefreshResponse {
+    private final String type = "Bearer";
+    private String accessToken;
+    private String refreshToken;
+
+    public TokenRefreshResponse(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+}
