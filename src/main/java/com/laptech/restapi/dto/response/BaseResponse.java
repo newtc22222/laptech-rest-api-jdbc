fb998c7d1102448566d6dee466116bfda0d42c61
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-02-08 22:49:49 +0700

    [DTO - Response] Build base class for response

---
+++ src/main/java/com/laptech/restapi/dto/response/BaseResponse.java
@@ -0,0 +1,35 @@
+package com.laptech.restapi.dto.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * @author Nhat Phi
+ * @since 2022-11-20
+ */
+
+public class BaseResponse {
+    private HttpStatus status;
+    private String message;
+
+    public BaseResponse(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
