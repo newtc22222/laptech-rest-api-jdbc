b15ec5bc38adceea770b9c6e0b2199e8d989039a
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-02-23 22:25:15 +0700

    [Model - LogSystem] Add new model LogSystem

---
+++ src/main/java/com/laptech/restapi/model/LogSystem.java
@@ -0,0 +1,36 @@
+package com.laptech.restapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Nhat Phi
+ * @since 2023-02-23
+ */
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LogSystem {
+    private long id;
+    private String actionTable;
+    private String recordId;
+    private LocalDateTime actionTime;
+    private String actionBy;
+    private String actionName;
+
+    @Override
+    public String toString() {
+        return "LogSystem{" +
+                "id=" + id +
+                ", actionTable='" + actionTable + '\'' +
+                ", recordId='" + recordId + '\'' +
+                ", actionDate=" + actionTime +
+                ", actionBy='" + actionBy + '\'' +
+                ", actionName='" + actionName + '\'' +
+                '}';
+    }
+}
