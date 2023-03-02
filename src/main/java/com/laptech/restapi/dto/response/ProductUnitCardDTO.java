c07c84e75e488852491f5f4eff13fd1be3b837a4
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-03-02 22:39:08 +0700

    [DTO - Response] Build ProductUnitCardDTO

---
+++ src/main/java/com/laptech/restapi/dto/response/ProductUnitCardDTO.java
@@ -0,0 +1,30 @@
+package com.laptech.restapi.dto.response;
+
+import com.laptech.restapi.model.Discount;
+import com.laptech.restapi.model.Product;
+import com.laptech.restapi.model.ProductImage;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+
+/**
+ * @author Nhat Phi
+ * @since 2023-03-02
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductUnitCardDTO {
+    private String id;
+    private String cartId;
+    private String invoiceId;
+    private Product product;
+    private ProductImage imageRepresent;
+    private Collection<Discount> discounts;
+    private BigDecimal discountPrice;
+}
