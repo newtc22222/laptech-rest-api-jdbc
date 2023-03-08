39930ecae871cc2e24524ba2786eba453cffed31
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-03-08 14:27:44 +0700

    [Util - ConvertDate] Modified name of function (Date -> LocalDate)

--- src/main/java/com/laptech/restapi/util/ConvertDate.java
+++ src/main/java/com/laptech/restapi/util/ConvertDate.java
@@ -3,6 +3,7 @@ package com.laptech.restapi.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Date;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
@@ -14,7 +15,7 @@ import java.time.format.DateTimeParseException;
 public class ConvertDate {
     private static final Logger logger = LoggerFactory.getLogger(ConvertDateTime.class);
 
-    public static LocalDate getDateFromString(String text) {
+    public static LocalDate getLocalDateFromString(String text) {
         if (text == null || text.equals("")) return null;
         try {
             return LocalDate.parse(text, DateTimeFormatter.ISO_LOCAL_DATE);
