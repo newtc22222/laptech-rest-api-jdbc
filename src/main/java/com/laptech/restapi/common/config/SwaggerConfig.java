20c1e9e20024d5bfc1143708ba1d2b5abc62c3be
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-03-08 14:38:44 +0700

    [Config - Swagger] Change swagger type (OS3 -> Swagger_2)

--- src/main/java/com/laptech/restapi/common/config/SwaggerConfig.java
+++ src/main/java/com/laptech/restapi/common/config/SwaggerConfig.java
@@ -9,6 +9,7 @@ import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -23,40 +24,9 @@ import java.util.List;
  * @since 2022-12-26
  */
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
     public static final String AUTHORIZATION_HEADER = "Authorization";
-
-    private ApiKey apiKeys() {
-        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
-    }
-
-    private List<SecurityContext> securityContextList() {
-        return Collections.singletonList(SecurityContext
-                .builder()
-                .securityReferences(securityReferenceList())
-                .build());
-    }
-
-    private List<SecurityReference> securityReferenceList() {
-        AuthorizationScope scope = new AuthorizationScope("global", "accessEverything");
-        return Collections.singletonList(
-                new SecurityReference("JWT", new AuthorizationScope[]{scope})
-        );
-    }
-
-    @Bean
-    public Docket ecommerceApi() {
-        return new Docket(DocumentationType.OAS_30)
-                .securityContexts(securityContextList())
-                .securitySchemes(Collections.singletonList(apiKeys()))
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.laptech.restapi.controller"))
-                .paths(PathSelectors.regex("/api/v1.*"))
-                .build()
-                .directModelSubstitute(LocalDate.class, java.sql.Date.class)
-                .directModelSubstitute(LocalDateTime.class, java.util.Date.class)
-                .apiInfo(apiInfo());
-    }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
@@ -68,5 +38,37 @@ public class SwaggerConfig {
                 .contact(new Contact("Nhat Phi", null, "sunflynf@gmail.com"))
                 .build();
     }
+
+    @Bean
+    public Docket ecommerceApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(Collections.singletonList(securityContextList()))
+                .securitySchemes(Collections.singletonList(new ApiKey("JWT", AUTHORIZATION_HEADER, "header")))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.laptech.restapi.controller"))
+                .paths(PathSelectors.regex("/api/v1.*"))
+                .build()
+                .directModelSubstitute(LocalDate.class, java.sql.Date.class)
+                .directModelSubstitute(LocalDateTime.class, java.util.Date.class);
+    }
+
+    private SecurityContext securityContextList() {
+        return SecurityContext
+                .builder()
+                .securityReferences(securityReferenceList())
+                .build();
+    }
+
+    private List<SecurityReference> securityReferenceList() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+
+
+
 }
 
