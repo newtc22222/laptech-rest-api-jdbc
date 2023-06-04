1b2727aa26407cc050b0187276bd42593eafb5ff
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-06-04 22:55:22 +0700

    [dao - collapse] Build update multiple data

--- src/main/java/com/laptech/restapi/dao/CollaborateDAO.java
+++ src/main/java/com/laptech/restapi/dao/CollaborateDAO.java
@@ -1,5 +1,7 @@
 package com.laptech.restapi.dao;
 
+import java.util.List;
+
 /**
  * collapse: discount, label
  *
@@ -7,7 +9,7 @@ package com.laptech.restapi.dao;
  * @since (2023 - 01 - 30) 2022-11-22
  */
 public interface CollaborateDAO<T1, T2> {
-    int insert(T1 item1_id, T2 item2_id);
-
-    int remove(T1 item1_id, T2 item2_id);
+    int insert(T1 item_root_id, T2 item_add_id);
+    int remove(T1 item_root_id, T2 item_remove_id);
+    int updateMultiple(T1 item_root_id, List<T2> item_add_ids, List<T2> item_remove_ids);
 }
--- src/main/java/com/laptech/restapi/dao/ProductImageDAO.java
+++ src/main/java/com/laptech/restapi/dao/ProductImageDAO.java
@@ -5,12 +5,15 @@ import com.laptech.restapi.dto.filter.ProductImageFilter;
 import com.laptech.restapi.model.ProductImage;
 
 import java.util.Collection;
+import java.util.List;
 
 /**
  * @author Nhat Phi
  * @since 2022-11-21
  */
 public interface ProductImageDAO extends BaseDAO<ProductImage, ProductImageFilter, String> {
+    Collection<String> updateMultipleImages(List<ProductImage> imageIdAddList, List<ProductImage> imageUpdateList, List<String> imageIdRemoveList);
+
     int updateUrlAndType(String imageId, String url, ImageType type, String updateBy);
 
     Collection<ProductImage> findProductImageByProductId(String productId);
