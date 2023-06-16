cc4ddf6bff9cabe37c85c9761a8a0caaa0be55a6
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2023-06-16 22:22:43 +0700

    [main - features] Demo code base for recommend system

--- src/main/java/com/laptech/restapi/controller/recommend/ProductRecommendController.java
+++ src/main/java/com/laptech/restapi/controller/recommend/ProductRecommendController.java
@@ -1,6 +1,9 @@
 package com.laptech.restapi.controller.recommend;
 
 import com.laptech.restapi.model.Product;
+import com.laptech.restapi.model.User;
+import com.laptech.restapi.service.DiscountService;
+import com.laptech.restapi.service.ProductService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Api(tags = "Recommend system API", value = "Product Recommend System Controller")
 @CrossOrigin
 @RestController
@@ -17,13 +26,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/products")
 @PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
 public class ProductRecommendController {
+    private final DiscountService discountService;
+    private final ProductService productService;
+
     @GetMapping("on-sale")
-    public ResponseEntity<Product> getProductOnSale() {
-        return null;
+    public ResponseEntity<Collection<Product>> getProductOnSale() {
+        Collection<Product> productOnSaleList = productService.findAll()
+                .stream()
+                // get product has discount on date
+                .filter(product -> discountService
+                        .getDiscountsOfProduct(product.getId())
+                        .stream()
+                        .anyMatch(discount -> discount.getAppliedDate().isBefore(LocalDateTime.now())
+                                && discount.getEndedDate().isAfter(LocalDateTime.now())))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(productOnSaleList);
     }
 
+    /**
+     * Use Slope one algorithm: Calculate frequencies, actions and rating for product / user
+     * -> recommend product to user
+     * @see <a href="https://www.baeldung.com/java-collaborative-filtering-recommendations">Baeldung.com | Collaborative Filtering</a>
+     */
     @GetMapping("recommend")
     public ResponseEntity<Product> getProductRecommend() {
+        // models
+        Map<User, HashMap<Product, Double>> data;
+
+        // differences and frequencies matrices
+
+        // predictions
+
         return null;
     }
 }
